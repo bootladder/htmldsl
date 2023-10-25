9ec1fb1909ac707e8c29a23f6c1440d2a8c3fed1
Steve Anderson <steve@bootladder.com>
2023-10-25 07:50:38 +0000

    fix deploy  paths

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,29 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+  ,
+    build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  },
+    server: {
+    // To serve files directly without prefix
+    proxy: {
+      //'/src': {
+      //  target: 'http://localhost:3000',
+      //  rewrite: (path) => path.replace(/^\/src/, '')
+      //}
+    }}
 })
